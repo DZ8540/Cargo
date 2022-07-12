d7ee203ad9bc59305df4e0f341394c58a5a52b67
DZ8540 <d.z.2021@inbox.ru>
2022-07-12 16:14:49 +0300

    Initialized app

---
+++ contracts/drive.ts
@@ -0,0 +1,15 @@
+/**
+ * Contract source: https://git.io/JBt3I
+ *
+ * Feel free to let us know via PR, if you find something broken in this contract
+ * file.
+ */
+
+// * Types
+import type { InferDisksFromConfig } from '@adonisjs/core/build/config'
+import type driveConfig from '../config/drive'
+// * Types
+
+declare module '@ioc:Adonis/Core/Drive' {
+  interface DisksList extends InferDisksFromConfig<typeof driveConfig> {}
+}
---
+++ contracts/hash.ts
@@ -0,0 +1,15 @@
+/**
+ * Contract source: https://git.io/Jfefs
+ *
+ * Feel free to let us know via PR, if you find something broken in this contract
+ * file.
+ */
+
+// * Types
+import type hashConfig from '../config/hash'
+import type { InferListFromConfig } from '@adonisjs/core/build/config'
+// * Types
+
+declare module '@ioc:Adonis/Core/Hash' {
+  interface HashersList extends InferListFromConfig<typeof hashConfig> {}
+}
